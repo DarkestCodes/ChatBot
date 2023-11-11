7352e9354f6fa59864ed7177e36b7b938653d25c
DarkestCodes <150299892+DarkestCodes@users.noreply.github.com>
2023-11-11 12:03:49 +0100

    Add files via upload

---
+++ Update/CheckUpdate.py
@@ -0,0 +1,36 @@
+import requests, os
+
+with open("..\\Version\Version.txt", "r") as f:
+    old_ver = f.read().strip()
+
+print('Checking For Updates')
+url = 'https://raw.githubusercontent.com/DarkestCodes/ChatBot/main/Version/Version.txt'
+
+# Downloading the file by sending the request to the URL
+req = requests.get(url)
+ 
+# Split URL to get the file name
+filename = "Version.txt"
+ 
+# Writing the file to the local file system
+with open(filename,'wb') as output_file:
+    output_file.write(req.content)
+with open("Version.txt", "r") as f:
+    new_ver = f.read().strip()
+if new_ver == old_ver:
+    print("You're UpToDate")
+    os.remove("Version.txt")
+else:
+    print('There is a New Update')
+    os.remove("Version.txt")
+    print("\n\nDo You Want To Update?\n\n")
+    ask = input("Y/N: ").lower()
+    if ask == "y":
+        if os.name == 'nt':
+            os.system('''python -u "Update.py"''')
+        else:
+            os.system('''python3 -u "Update.py"''')
+    else:
+        print("Update was Canceled")
+        input
+        exit(0)
